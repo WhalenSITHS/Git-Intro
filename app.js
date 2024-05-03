d15a5f955f23cb3912c0a6aeebb47fa82e9ed12e
mr. whalen <michael.whalen@sitechhs.com>
2024-05-03 13:37:25 -0400

    ghjk

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log("hello")
+console.log(Number("1") + Number("1"));
+console.log(1 + 1);
---
+++ app.py
@@ -0,0 +1,5 @@
+accounts = [["1","2","8"], ["4","5","6","1"], ["0","7","4", "5","4"]]
+y = [[int(i) for i in x] for x in accounts]
+print(y)
+def getMax(accounts):
+    ###do the thing
